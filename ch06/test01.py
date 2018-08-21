ee54237324fe19e76fae4c8b3ea278798435a244
dongliyang <dongliyang2008@gmail.com>
2018-08-21 11:43:58 +0800

    ch06

---
+++ ch06/test01.py
@@ -0,0 +1,8 @@
+favorite_languages = {
+    'jen':'python',
+    'sarah':'c',
+    'edward':'ruby',
+    'phil':'python'
+}
+
+print("Sarah's favorite lanuage is " + favorite_languages['sarah'].title() + ".")
---
+++ ch06/test02.py
@@ -0,0 +1,26 @@
+favorite_languages = {
+    'jen':'python',
+    'sarah':'c',
+    'edward':'ruby',
+    'phil':'python'
+}
+
+# for name,language in favorite_languages.items():
+#     print(name.title() + "'s favorite language is " + language.title() + ".")
+
+
+# for name in favorite_languages.keys():
+#     print(name.title())
+
+# for name in sorted(favorite_languages.keys()):
+#     print(name.title())
+
+# for language in favorite_languages.values():
+#     print(language.title())
+
+# for language in set(favorite_languages.values()):
+#     print(language.title())
+
+# if 'erin' not in favorite_languages.keys():
+#     print("Erin,please take our poll!")
+
